d0d613723f176221930a25cb05be4da06c168bc2
loux22 <louis.ardilly@yahoo.fr>
2020-05-14 17:43:23 +0200

    add fixtures and message

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,15 @@ from app import config
 from app.models import db
 
 from app.controller import user
+from app.controller import message
+from app.controller import fixtures
 
 app = Flask(__name__)
 app.config.from_object(config.Config)
 
 app.register_blueprint(user.main)
+app.register_blueprint(message.message)
+app.register_blueprint(fixtures.fixtures)
 db.init_app(app)
 
 # with app.app_context():
