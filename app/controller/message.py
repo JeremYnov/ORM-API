df88162b544aaa47400f5d71d4e11543a916067a
loux22 <louis.ardilly@yahoo.fr>
2020-05-14 20:01:36 +0200

    add message whitout form

--- app/controller/message.py
+++ app/controller/message.py
@@ -8,20 +8,34 @@ message = Blueprint('message', __name__, url_prefix='/')
 def list_message():
     # on recupere le user connecter
     userLog = User.query.filter_by(id=1).first()
-    Messages = db.session.query(Message).filter(or_(Message.send_by_id==1, Message.receive_by_id==1)).all()
+    messages = db.session.query(Message).filter(or_(Message.send_by_id==1, Message.receive_by_id==1)).order_by(Message.set_date).all()
     listMessage = []
-    for message in Messages:
+    statut = False
+    for message in messages:
+        statut = False
+        if not(listMessage):
+                listMessage.append(message) 
         for value in listMessage:
             if userLog == message.send_by:
-                if message.receive_by != value.receive_by:
-                    listMessage.append(message)
+                if message.receive_by == value.receive_by and message.send_by == value.send_by:
+                    statut = True
+                if message.send_by == value.receive_by and message.receive_by == value.send_by:
+                    statut = True    
             else:
-                if message.send_by != value.send_by:
-                    listMessage.append(message)
+                if message.send_by == value.send_by and message.receive_by == value.receive_by:
+                    statut = True
+                if message.send_by == value.receive_by and message.receive_by == value.send_by:
+                    statut = True
+
+        if statut == False :      
+            listMessage.append(message)      
+            
                 
 
-    return render_template('pages/message/listMessage.html', listMessage=Messages, userLog=userLog.id, m=listMessage)
+    return render_template('pages/message/listMessage.html', listMessage=listMessage, userLog=userLog)
 
 @message.route('/talk/<int:id>')
 def talk(id):
-    return render_template('pages/message/talk.html', id=id)    
+    userLog = User.query.filter_by(id=1).first()
+    messages = db.session.query(Message).filter(or_(Message.send_by_id==id, Message.receive_by_id==id)).filter(or_(Message.send_by_id==userLog.id, Message.receive_by_id==userLog.id)).order_by(Message.set_date).all()
+    return render_template('pages/message/talk.html', messages=messages, userLog=userLog)    
