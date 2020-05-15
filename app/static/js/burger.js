ef3e4591a95bb3376c6228a62d323a9ee040a6e0
Jérémy <jeremy.richard@ynov.com>
2020-05-15 17:59:26 +0200

    adding correction of the navbar

--- app/static/js/burger.js
+++ app/static/js/burger.js
@@ -1,38 +1,46 @@
-jQuery(function ($) {
+// jQuery(function ($) {
 
-    $('.header-nav-wrapper').on('click', function () {
-        $('.overlay').toggle();
+//     $('.header-nav-wrapper').on('click', function () {
+//         $('.overlay').toggle();
 
-        setTimeout(function () {
-            $('.overlay').addClass('is-open')
-        }, 200)
+//         setTimeout(function () {
+//             $('.overlay').addClass('is-open')
+//         }, 200)
 
-        $('.body').addClass('overflow')
-        $('.nav-responsive').addClass('nav-transition')
-        $('.header-nav-burger').addClass('is-animate')
-    })
+//         $('.body').addClass('overflow')
+//         $('.nav-responsive').addClass('nav-transition')
+//         $('.header-nav-burger').addClass('is-animate')
+//     })
 
-    $('.overlay').on('click', function () {
+//     $('.overlay').on('click', function () {
 
-        setTimeout(function () {
-            $('.overlay').removeClass('is-open')
-            $('.overlay').toggle();
-        }, 500)
+//         setTimeout(function () {
+//             $('.overlay').removeClass('is-open')
+//             $('.overlay').toggle();
+//         }, 500)
         
-        $('.body').removeClass('overflow')
-        $('.nav-responsive').removeClass('nav-transition')
-        $('.header-nav-burger').removeClass('is-animate')
-    })
+//         $('.body').removeClass('overflow')
+//         $('.nav-responsive').removeClass('nav-transition')
+//         $('.header-nav-burger').removeClass('is-animate')
+//     })
 
-    var heroHeight,headerHeight,windowHeight;
+//     var heroHeight,headerHeight,windowHeight;
 
-    $(window).on('resize',function(){
-        headerHeight = $('.header-main').height();
-        windowHeight = $(window).height();
-        heroHeight = windowHeight - headerHeight
-        $('.hero-home').css('height',heroHeight)
-        console.log(heroHeight)
-    })
+//     $(window).on('resize',function(){
+//         headerHeight = $('.header-main').height();
+//         windowHeight = $(window).height();
+//         heroHeight = windowHeight - headerHeight
+//         $('.hero-home').css('height',heroHeight)
+//         console.log(heroHeight)
+//     })
 
 
-})
+// })
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 10) {
+        $('.header-main').css('box-shadow','0px 1px 10px #999');
+    } else {
+        $('.header-main').css('box-shadow','none');
+    }
+});
