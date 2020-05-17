5a77e72975ec22c27c6136a0e02c9140fd3b8bac
Jérémy <jeremy.richard@ynov.com>
2020-05-17 14:21:47 +0200

    adding style for signup page

--- app/static/js/burger.js
+++ app/static/js/burger.js
@@ -18,29 +18,45 @@
 //             $('.overlay').removeClass('is-open')
 //             $('.overlay').toggle();
 //         }, 500)
-        
+
 //         $('.body').removeClass('overflow')
 //         $('.nav-responsive').removeClass('nav-transition')
 //         $('.header-nav-burger').removeClass('is-animate')
 //     })
-
-//     var heroHeight,headerHeight,windowHeight;
-
-//     $(window).on('resize',function(){
-//         headerHeight = $('.header-main').height();
-//         windowHeight = $(window).height();
-//         heroHeight = windowHeight - headerHeight
-//         $('.hero-home').css('height',heroHeight)
-//         console.log(heroHeight)
-//     })
-
-
 // })
 
-$(window).scroll(function() {
-    if ($(window).scrollTop() > 10) {
-        $('.header-main').css('box-shadow','0px 1px 10px #999');
-    } else {
-        $('.header-main').css('box-shadow','none');
-    }
-});
+jQuery(function ($) {
+    console.log("Test")
+    var wrapperHeight, headerHeight, windowHeight;
+    console.log("Test")
+    $(window).on('resize', function () {
+        console.log("Test")
+        headerHeight = $('.header-main').height();
+        windowHeight = $(window).height();
+        wrapperHeight = windowHeight - headerHeight
+        $('.wrapper').css('height', heroHeight)
+        console.log(heroHeight)
+    })
+})
+
+
+// $(window).scroll(function() {
+//     if ($(window).scrollTop() > 10) {
+//         $('.header-main').css('box-shadow','0px 1px 10px #999');
+//     } else {
+//         $('.header-main').css('box-shadow','none');
+//     }
+// });
+
+
+var headerHeight;
+var screenHeight;
+function resize() {
+    screenHeight = $(window).innerHeight();
+    headerHeight = $('.header-main').innerHeight();
+    valueForm = screenHeight - headerHeight;
+    $('.wrapper').css('height', valueForm);
+}
+
+resize();
+$(window).resize(resize);
