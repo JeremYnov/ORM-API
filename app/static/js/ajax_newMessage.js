b80780cb475f4e2cf722b46f1ee470b484776c72
Jérémy <jeremy.richard@ynov.com>
2020-05-18 18:06:36 +0200

    Style push

--- app/static/js/ajax_newMessage.js
+++ app/static/js/ajax_newMessage.js
@@ -9,12 +9,12 @@ $(document).ready(function(){
                 if(res != ""){
                     $('.result_user_js').html("");
                     $.each(res,function(index,value) {          
-                        data = '<div><img height="20px" width="20px" src="../../../static/img/' + value.avatar + '" alt=""><a href="/talk/' + value.id + '">' + value.username + '</a></div>'
+                        data = '<div class="search-user"><img height="20px" width="20px" src="../../../static/img/' + value.avatar + '" alt=""><a href="/talk/' + value.id + '">' + value.username + '</a></div>'
                         $('.result_user_js').append(data);
                     })
                 }else{
                     console.log(1)
-                    $('.result_user_js').html("<p>aucun utilisateurs</p>") ;
+                    $('.result_user_js').html("<div class='alert alert-warning' role='alert'>Aucun utilisateurs</div>") ;
                 }
             }
         })
