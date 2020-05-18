88427247b2db65a60cfa1c35ca64705ac7f23f83
loux22 <louis.ardilly@yahoo.fr>
2020-05-18 13:04:07 +0200

    searchbar profil

---
+++ app/static/js/ajax_searchProfil.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $('.search_profil-js').on("input", function (e) {
+        user = $('.search_profil-js').val()
+        $.ajax({
+            method: "post",
+            url: "/user",
+            data: { text: user },
+            success: function (res) {
+                if (user == "") {
+                    $('.profil_user-js').html("");
+                } else if (res != "") {
+                    $('.profil_user-js').html("");
+                    $.each(res, function (index, value) {
+                        data = '<div><img height="20px" width="20px" src="../../../static/img/' + value.avatar + '" alt=""><a href="/profil/' + value.id + '">' + value.username + '</a></div>'
+                        $('.profil_user-js').append(data);
+                    })
+                }
+            }
+        })
+    })
+})
